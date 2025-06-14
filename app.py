587ec4daf998c34bb64ac5a57b7a9179893f2807
Shilpana <bhawal.shilpana@gmail.com>
2025-06-14 15:39:21 +0530

    Second commit

---
+++ app.py
@@ -0,0 +1,10 @@
+def addition(a,b):
+    return a + b
+
+addition(1,2)
+
+def subtraction(a, b):
+    return a - b
+
+print("Addition result:", addition(1, 2))
+print("Subtraction result:", subtraction(5, 3))
